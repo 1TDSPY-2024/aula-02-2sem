808ef9526dc2d50e7eee7a2ed858169e310ed65b
felipeSeiki <felipe.hashiguti@gmail.com>
2024-08-09 21:54:16 -0300

    Comecando exercicio 3

--- js/script.js
+++ js/script.js
@@ -29,37 +29,38 @@
 // const nomesAlunosVerificados = objAlunos.some(a => a.nome.includes("e"));
 // console.log(nomesAlunosVerificados);
 
-let objAlunos = [
-    {id:1,nome:"Felipe",rm:98985},
-    {id:2,nome:"Lucas",rm:98986},
-    {id:3,nome:"Matheus",rm:98987},
-    {id:4,nome:"Augusto",rm:98988},
-    {id:5,nome:"Diego",rm:98989},
-];
+// let objAlunos = [
+//     {id:1,nome:"Felipe",rm:98985},
+//     {id:2,nome:"Lucas",rm:98986},
+//     {id:3,nome:"Matheus",rm:98987},
+//     {id:4,nome:"Augusto",rm:98988},
+//     {id:5,nome:"Diego",rm:98989},
+// ];
 
-let objNotas = [
-    {id:1,nota: 8.9},
-    {id:2,nota: 7.4},
-    {id:3,nota: 9.2},
-    {id:4,nota: 5.1},
-    {id:5,nota: 3.7},
-];
+// let objNotas = [
+//     {id:1,nota: 8.9},
+//     {id:2,nota: 7.4},
+//     {id:3,nota: 9.2},
+//     {id:4,nota: 5.1},
+//     {id:5,nota: 3.7},
+// ];
 
-//Gerar novo objeto a partir dos alunos e notas com MAP
+// //Gerar novo objeto a partir dos alunos e notas com MAP
 
-const alunosComNotas = objAlunos.map(a =>{
-    //retornando um obj nota de acordo com o ID do aluno
-    const nota = objNotas.find(n => n.id === a.id);
-    //Montando o nobo objeto aluno com notas para retorno na array
-    // return {id:a.id, nome:a.nome, rm:a.rm, nota:nota.nota};
-    return {...a, nota:nota.nota};
-});
+// const alunosComNotas = objAlunos.map(a =>{
+//     //retornando um obj nota de acordo com o ID do aluno
+//     const nota = objNotas.find(n => n.id === a.id);
+//     //Montando o nobo objeto aluno com notas para retorno na array
+//     // return {id:a.id, nome:a.nome, rm:a.rm, nota:nota.nota};
+//     return {...a, nota:nota.nota};
+// });
 
-console.log(alunosComNotas);
+// console.log(alunosComNotas);
 
-//utilizando o reduce ppara consolidar o total das notas dos alunos
-//o 0 é o valor inicial, necessario para comecar
-const somaDasNotas = alunosComNotas.reduce((acumulado, alunos) =>{
-    return(acumulado + alunos.nota);
-},0);
-console.log(somaDasNotas);
+// //utilizando o reduce ppara consolidar o total das notas dos alunos
+// //o 0 é o valor inicial, necessario para comecar
+// const somaDasNotas = alunosComNotas.reduce((acumulado, alunos) =>{
+//     return(acumulado + alunos.nota);
+// },0);
+// console.log(somaDasNotas);
+
