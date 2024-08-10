6aff9b7e43534edab10465565e23426b8349983b
difurigo <furigo9diego@gmail.com>
2024-08-09 21:23:16 -0300

    aula_2

---
+++ js/exercicio3.js
@@ -0,0 +1,14 @@
+const objSalarios = [
+    { id: 1, salario: 2450 },
+    { id: 2, salario: 3700 },
+    { id: 3, salario: 5250 },
+    { id: 4, salario: 4100 },
+    { id: 5, salario: 6800 },
+    { id: 6, salario: 3900 },
+    { id: 7, salario: 7550 },
+    { id: 8, salario: 2900 },
+    { id: 9, salario: 4750 },
+    { id: 10, salario: 5600 },
+];
+
+const salariosAtualizados = 0;
